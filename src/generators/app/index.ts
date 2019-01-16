a812901e1ea32e6181e838f9ffacfb30df97ffec
Max Ewing <max.ewing@capgemini.com>
2019-01-16 10:41:36 +0000

    Updated to TypeScript and finished templating logic

--- src/generators/app/index.ts
+++ src/generators/app/index.ts
@@ -1,120 +1,143 @@
 "use strict";
 import Generator = require("yeoman-generator");
-import yosay = require("yosay");
+import yosay from "yosay";
 import chalk = require("chalk");
 import inquirer = require("inquirer");
-import { readdirSync } from "fs";
 import rimraf = require("rimraf");
+import Renamer from "renamer";
 
 module.exports = class extends Generator {
-    answers!: inquirer.Answers;
-    async prompting(): Promise<void> {
-        this.log(
-            yosay(
-                `Welcome to the laudable ${chalk.default.red(
-                    "cdspackage"
-                )} generator!`
-            )
-        );
+  answers!: inquirer.Answers;
+  async prompting(): Promise<void> {
+    this.log(
+      yosay(
+        `Welcome to the laudable ${chalk.default.red("cdspackage")} generator!`
+      )
+    );
 
-        const prompts: Generator.Questions = [
-            {
-                type: "input",
-                name: "client",
-                message: "What is the name of the client?",
-                validate: validateNamespace,
-                transformer: transformNamespace
+    const prompts: Generator.Questions = [
+      {
+        type: "input",
+        name: "client",
+        message: "What is the name of the client?",
+        validate: validateNamespace,
+        filter: filterNamespace,
+        store: true
+      },
+      {
+        type: "input",
+        name: "package",
+        message: "What is the name of the package?",
+        validate: validateNamespace,
+        filter: filterNamespace,
+        store: true
+      },
+      {
+        type: "input",
+        name: "devUrl",
+        message: "What is the dev URL?",
+        validate: validateUrl,
+        store: true
+      },
+      {
+        type: "input",
+        name: "devUsername",
+        message: "What is the dev admin email?",
+        validate: validateEmail,
+        store: true
+      },
+      {
+        type: "input",
+        name: "devPassword",
+        message: "What is the dev admin password?",
+        store: true
+      },
+      {
+        type: "input",
+        name: "dataUrl",
+        message: "What is the master data URL?",
+        validate: validateUrl,
+        store: true
+      },
+      {
+        type: "input",
+        name: "dataUsername",
+        message: "What is the master data admin email?",
+        validate: validateEmail,
+        store: true
+      },
+      {
+        type: "input",
+        name: "dataPassword",
+        message: "What is the master data admin password?",
+        store: true
+      }
+    ];
 
-            },
-            {
-                type: "input",
-                name: "package",
-                message: "What is the name of the package?",
-                validate: validateNamespace,
-                transformer: transformNamespace
-            },
-            {
-                type: "input",
-                name: "devUrl",
-                message: "What is the dev URL?",
-                validate: validateUrl
-            },
-            {
-                type: "input",
-                name: "devUsername",
-                message: "What is the dev admin email?",
-                validate: validateEmail
-            },
-            {
-                type: "input",
-                name: "devPassword",
-                message: "What is the dev admin password?",
-            },
-            {
-                type: "input",
-                name: "dataUrl",
-                message: "What is the master data URL?",
-                validate: validateUrl
-            },
-            {
-                type: "input",
-                name: "dataUsername",
-                message: "What is the master data admin email?",
-                validate: validateEmail,
-            },
-            {
-                type: "input",
-                name: "dataPassword",
-                message: "What is the master data admin password?",
-            },
-        ];
+    this.answers = await this.prompt(prompts);
+  }
 
-        this.answers = await this.prompt(prompts);
-    }
-
-    async writing(): Promise<void> {
-        this.log(`Downloading latest template to ${this.templatePath()}`);
-        this.spawnCommandSync("git", [
-            "clone",
-            "--depth=1",
-            "--single-branch",
-            "--branch", "sample-repository",
-            "-q",
-            "https://capgeminiuk.visualstudio.com/Capgemini%20Reusable%20IP/_git/Capgemini.Xrm.Templates",
-            this.templatePath()
-        ]);
-        rimraf.sync(`${this.templatePath}/.git`);
-
-        const outputPath: string =
-            isDirectoryEmpty(this.destinationPath()) || (this.sourceRoot() === this.destinationPath()) ?
-                "" : `${this.answers.client}.${this.answers.package}`;
-        this.log("Building project to: " + this.destinationPath(outputPath));
-        this.fs.copyTpl(this.templatePath(), this.destinationPath(outputPath), this.answers);
-    }
+  async writing(): Promise<void> {
+    this.log(`Downloading latest template to ${this.templatePath()}`);
+    rimraf.sync(this.templatePath());
+    this.spawnCommandSync("git", [
+      "clone",
+      "--depth=1",
+      "--single-branch",
+      "--branch",
+      "sample-repository",
+      "-q",
+      "https://capgeminiuk.visualstudio.com/Capgemini%20Reusable%20IP/_git/Capgemini.Xrm.Templates",
+      this.templatePath()
+    ]);
+    rimraf.sync(`${this.templatePath()}/.git`);
+    const renamer = new Renamer();
+    renamer.rename({
+      files: [`${this.templatePath()}/**/*`],
+      find: "Client.Package",
+      replace: `${this.answers.client}.${this.answers.package}`,
+      dryRun: false
+    });
+    this.fs.copyTpl(this.templatePath(), this.destinationPath(), this.answers);
+    this.fs.copy(
+      this.templatePath(
+        `Solutions\\${this.answers.client}.${
+          this.answers.package
+        }.Customisations\\Scripts\\.npmrc`
+      ),
+      `Solutions\\${this.answers.client}.${
+        this.answers.package
+      }.Customisations\\Scripts\\.npmrc`
+    );
+  }
 };
 
 function validateNamespace(input: string): boolean | string {
-    return /^[a-zA-Z ]+$/.test(input) ? true : `Answer must not contain numeric or special characters.`;
+  return /^[a-zA-Z]+$/.test(input)
+    ? true
+    : `Answer must not contain spaces, numeric characters or special characters.`;
 }
 
-function transformNamespace(input: string): string {
-    return input.toLowerCase()
-        .split(" ")
-        .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
-        .join();
+function filterNamespace(input: string): string {
+  return input
+    .split(" ")
+    .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+    .join();
 }
 
 function validateUrl(input: string): boolean | string {
-    return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(input) ?
-        true : "You must provide a valid URL.";
+  return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(
+    input
+  )
+    ? true
+    : "You must provide a valid URL.";
 }
 
 function validateEmail(input: string): boolean | string {
-    // tslint:disable-next-line:max-line-length
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(input) ?
-        true : "You must provide a valid email.";
-}
-
-function isDirectoryEmpty(dirname: string): boolean {
-    return !readdirSync(dirname).length;
+  // tslint:disable-next-line:max-line-length
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    input
+  )
+    ? true
+    : "You must provide a valid email.";
 }
---
+++ typings/renamer.d.ts
@@ -0,0 +1,11 @@
+declare module "renamer" {
+  export interface RenameOptions {
+    files: string[];
+    find: string;
+    replace: string;
+    dryRun: boolean;
+  }
+  export default class Renamer {
+    public rename(options: RenameOptions): void;
+  }
+}
