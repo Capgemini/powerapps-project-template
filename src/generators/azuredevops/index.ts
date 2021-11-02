4ef5de94478b3971978eb0c717e538e80ef80bb8
Tom Ashworth <tom.ashworth@capgemini.com>
2021-11-02 08:50:56 +0000

    show both generation and rollback error

--- src/generators/azuredevops/index.ts
+++ src/generators/azuredevops/index.ts
@@ -1,4 +1,5 @@
 import { getPersonalAccessTokenHandler, WebApi } from 'azure-devops-node-api';
+import chalk from 'chalk';
 import inquirer from 'inquirer';
 import Generator from 'yeoman-generator';
 import {
@@ -151,10 +152,16 @@ class Main extends Generator {
         tenantId: this.answers.tenantId,
       });
       this.log('Done.');
-    } catch (e) {
-      this.log('Package generator encountered an error.');
-      await scaffolder.rollback(this.answers.adoProject);
-      this.log(e as string);
+    } catch (scaffoldError) {
+      this.log(chalk.red('Package generator encountered an error.'));
+      try {
+        await scaffolder.rollback(this.answers.adoProject);
+      } catch (rollbackError) {
+        this.log(chalk.red('Rollback failed: '));
+        this.log(chalk.red(rollbackError as string));
+        this.log(chalk.red('--------------------------------------'));
+      }
+      this.log(chalk.red(scaffoldError as string));
     }
   }
 
