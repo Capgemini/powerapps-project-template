198a7b5ba6d8bf7eca7de62a6c5cf14a7b6f3a34
Tom Ashworth <thomas.d.ashworth@gmail.com>
2021-11-16 22:56:08 +0000

    feat: skip pac auth question when generating via chatbot

--- src/generators/solution/index.ts
+++ src/generators/solution/index.ts
@@ -1,10 +1,10 @@
-import { raw } from 'guid';
-import inquirer from 'inquirer';
-import Renamer from 'renamer';
-import stripJsonComments from 'strip-json-comments';
-import { Builder, parseString } from 'xml2js';
-import Generator from 'yeoman-generator';
-import { validatePacAuthProfile } from '../../common/utilities';
+import { raw } from "guid";
+import inquirer from "inquirer";
+import Renamer from "renamer";
+import stripJsonComments from "strip-json-comments";
+import { Builder, parseString } from "xml2js";
+import Generator from "yeoman-generator";
+import { validatePacAuthProfile } from "../../common/utilities";
 
 class Main extends Generator {
   private answers!: inquirer.Answers;
@@ -12,53 +12,56 @@ class Main extends Generator {
   public async prompting(): Promise<void> {
     this.answers = await this.prompt([
       {
-        message: 'Publisher prefix?',
-        name: 'prefix',
+        message: "Publisher prefix?",
+        name: "prefix",
         store: true,
       },
       {
-        message: 'Name of the client?',
-        name: 'client',
+        message: "Name of the client?",
+        name: "client",
         store: true,
       },
       {
-        message: 'Name of the package?',
-        name: 'package',
+        message: "Name of the package?",
+        name: "package",
         store: true,
       },
       {
-        message: 'Name of the solution?',
-        name: 'solution',
+        message: "Name of the solution?",
+        name: "solution",
         store: true,
       },
       {
-        message: 'Name of PAC Auth profile? This is used to export the solution locally. (please ensure this has been created with pac auth create -n <name> -u <url>)',
-        name: 'pacProfile',
+        message:
+          "Name of PAC Auth profile? This is used to export the solution locally. (please ensure this has been created with pac auth create -n <name> -u <url>)",
+        name: "pacProfile",
         store: false,
         validate: validatePacAuthProfile,
+        when: !this.options?.chatbot,
       },
       {
-        message: 'Development environment URL?',
-        name: 'environment',
+        message: "Development environment URL?",
+        name: "environment",
         store: false,
       },
       {
-        message: 'Are changes to this solution promoted using a staging environment?',
-        name: 'hasStagingEnvironment',
+        message:
+          "Are changes to this solution promoted using a staging environment?",
+        name: "hasStagingEnvironment",
         store: false,
-        type: 'confirm',
+        type: "confirm",
       },
       {
-        message: 'Staging environment URL?',
-        name: 'stagingEnvironment',
+        message: "Staging environment URL?",
+        name: "stagingEnvironment",
         store: false,
         when: (answers: any) => answers.hasStagingEnvironment,
       },
     ]);
 
-    this.answers.client = this.answers.client.replace(/\s/g, '');
-    this.answers.package = this.answers.package.replace(/\s/g, '');
-    this.answers.solution = this.answers.solution.replace(/\s/g, '');
+    this.answers.client = this.answers.client.replace(/\s/g, "");
+    this.answers.package = this.answers.package.replace(/\s/g, "");
+    this.answers.solution = this.answers.solution.replace(/\s/g, "");
     this.answers.solutionUniqueName = `${this.answers.prefix}_${this.answers.package}_${this.answers.solution}`;
     this.answers.projectGuid = raw();
   }
@@ -72,26 +75,26 @@ class Main extends Generator {
 
   public async install() {
     this.renameFileAndFolders([
-      { from: '{{Client}}', to: this.answers.client },
-      { from: '{{Package}}', to: this.answers.package },
-      { from: '{{Solution}}', to: this.answers.solution },
-      { from: '{{prefix}}', to: this.answers.prefix },
+      { from: "{{Client}}", to: this.answers.client },
+      { from: "{{Package}}", to: this.answers.package },
+      { from: "{{Solution}}", to: this.answers.solution },
+      { from: "{{prefix}}", to: this.answers.prefix },
     ]);
   }
 
   private writeSource = () => {
-    this.log('Writing solution from template...');
+    this.log("Writing solution from template...");
     this.fs.copyTpl(
-      this.templatePath('source'),
-      this.destinationPath('src', 'solutions'),
+      this.templatePath("source"),
+      this.destinationPath("src", "solutions"),
       this.answers,
       {},
-      { globOptions: { dot: true } },
+      { globOptions: { dot: true } }
     );
   };
 
   private writeSolutionConfig = () => {
-    this.log('Writing solution configuration...');
+    this.log("Writing solution configuration...");
 
     const solutionConfig: any = {};
 
@@ -103,33 +106,37 @@ class Main extends Generator {
     }
 
     this.fs.writeJSON(
-      this.destinationPath('src', 'solutions', '{{prefix}}_{{Package}}_{{Solution}}', 'solution.json'),
-      solutionConfig,
+      this.destinationPath(
+        "src",
+        "solutions",
+        "{{prefix}}_{{Package}}_{{Solution}}",
+        "solution.json"
+      ),
+      solutionConfig
     );
   };
 
   private updateTasks = () => {
-    this.log('Updating tasks.json...');
+    this.log("Updating tasks.json...");
     const tasksString = this.fs.read(
-      this.destinationPath('.vscode', 'tasks.json'),
+      this.destinationPath(".vscode", "tasks.json")
     );
     const tasks = JSON.parse(stripJsonComments(tasksString));
     tasks.inputs
-      .find((input: { id: string }) => input.id === 'solution')
+      .find((input: { id: string }) => input.id === "solution")
       .options.push(
-        `${this.answers.prefix}_${this.answers.package}_${this.answers.solution
-        }`,
+        `${this.answers.prefix}_${this.answers.package}_${this.answers.solution}`
       );
-    this.fs.writeJSON(this.destinationPath('.vscode', 'tasks.json'), tasks);
+    this.fs.writeJSON(this.destinationPath(".vscode", "tasks.json"), tasks);
   };
 
   private updateImportConfig = async () => {
-    this.log('Updating import config to include new solution.');
+    this.log("Updating import config to include new solution.");
 
     const importConfigPath = this.destinationPath(
-      'deploy',
-      'PkgFolder',
-      'ImportConfig.xml',
+      "deploy",
+      "PkgFolder",
+      "ImportConfig.xml"
     );
     parseString(
       this.fs.read(importConfigPath),
@@ -142,12 +149,12 @@ class Main extends Generator {
         const solutionFullName = `${this.answers.prefix}_${this.answers.package}_${this.answers.solution}`;
         const solutionElement = {
           $: {
-            deleteonly: 'false',
-            forceUpgrade: 'false',
+            deleteonly: "false",
+            forceUpgrade: "false",
             overwriteunmanagedcustomizations: true,
             publishworkflowsandactivateplugins: true,
             solutionpackagefilename: `${solutionFullName}/${solutionFullName}.zip`,
-            useAsync: 'true',
+            useAsync: "true",
           },
         };
 
@@ -168,16 +175,16 @@ class Main extends Generator {
             includeWhiteChars: false,
             renderOpts: { pretty: true },
             trim: true,
-          }).buildObject(res),
+          }).buildObject(res)
         );
-      },
+      }
     );
   };
 
   private renameFileAndFolders = (
-    rules: Array<{ from: string; to: string }>,
+    rules: Array<{ from: string; to: string }>
   ) => {
-    this.log('Renaming file and folders...');
+    this.log("Renaming file and folders...");
 
     const renamer = new Renamer();
 
