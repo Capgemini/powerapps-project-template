158ed7c1cbe0720a57d98f7eadcfc8a6bdb2637a
Krutarth Patel <krutarthppp@gmail.com>
2022-09-12 20:07:06 +0530

    Update RepoGenerator.ts
    Fixed lint package formatting issue

--- src/generators/azuredevops/generator/RepoGenerator.ts
+++ src/generators/azuredevops/generator/RepoGenerator.ts
@@ -41,9 +41,9 @@ export default class RepoGenerator implements IGenerator<GitRepository> {
     }
 
     const orgName : string = repo.remoteUrl!.startsWith('https://dev.azure.com/')
-        ? repo.remoteUrl!.split('/')[3] // for https://dev.azure.com/{orgname}/
-        : repo.remoteUrl!.split('/')[2].split('.')[0]; // for https://{orgname}.visualstudio.com/
-    const repoUrl = `https://${orgName}:${adoToken}@dev.azure.com/${orgName}/${encodeURIComponent(projectName)}/_git/${repoName}`;   
+      ? repo.remoteUrl!.split('/')[3] // for https://dev.azure.com/{orgname}/
+      : repo.remoteUrl!.split('/')[2].split('.')[0]; // for https://{orgname}.visualstudio.com/
+    const repoUrl = `https://${orgName}:${adoToken}@dev.azure.com/${orgName}/${encodeURIComponent(projectName)}/_git/${repoName}`;
     await this.pushRepo(repoLocation, repoUrl);
 
     return repo;
